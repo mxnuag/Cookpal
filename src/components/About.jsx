62771fb36b778b738eb38f822cdc1d0ad4aadcfa
AGARWALMANU3008 <manuagarwal3008@gmail.com>
2024-07-19 23:09:09 +0530

    first

---
+++ src/components/About.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+      <article className='about'>
+        <h1>CookPal</h1>
+        <p>Welcome to CookPal, your ultimate destination for discovering and preparing delicious dishes from around the world. Whether you're a seasoned chef or just starting your culinary journey, our app is designed to make cooking fun, easy, and accessible for everyone.</p>
+
+        <p>What We Offer</p>
+
+        <p>Diverse Recipes: Explore a wide variety of dishes, from classic comfort foods to exotic international cuisines. Our curated collection includes something for every taste and occasion.
+
+          Detailed Instructions: Each recipe comes with step-by-step instructions to guide you through the cooking process, ensuring you get perfect results every time.
+
+          Ingredients List: Find a comprehensive list of ingredients needed for each dish, so you can prepare in advance and shop with ease.
+
+          Cooking Times: Know exactly how long each recipe will take, helping you plan your meals more efficiently.</p>
+        <p>Our Mission</p>
+
+        <p>At CookPal, we believe that cooking should be a joyful and rewarding experience. Our mission is to inspire and empower you to create delicious meals at home, using easy-to-follow recipes and quality ingredients.</p>
+        <p>Get Cooking!</p>
+
+        <p>Dive into our recipe collection, try new dishes, and bring the joy of cooking into your home. We are here to support you on your culinary adventure with helpful tips, tricks, and a community of food lovers.
+
+Happy Cooking!
+
+</p>
+      </article>
+    </>
+  )
+}
+
+export default About
